9c89bfa15ee7552f4e826e619c297dc9be5fe3ae
Frah01 <vinario.francesco@gmail.com>
2023-03-13 14:25:08 +0100

    Aggiunta route , controller e validator formcontac

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProjectController as ProjectController;
+use App\Http\Controllers\Api\GuestLeadController as GuestLeadController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/projects', [ProjectController::class, 'index']);
 Route::get('/projects/{slug}', [ProjectController::class, 'show']);
+Route::post('/contacts', [GuestLeadController::class, 'store']);
